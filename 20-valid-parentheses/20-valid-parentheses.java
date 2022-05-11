343b3b1f5b84ee5d16bd1a2c39edd185fdd27afb
Breanna Thompson <35754765+bwolfee10@users.noreply.github.com>
2022-05-11 09:29:58 -0500

    Time: 2 ms (80.94%), Space: 40 MB (97.50%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')','(');
+        closeToOpen.put(']','[');
+        closeToOpen.put('}','{');
+
+        for (char c: s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+                }
+                else return false;
+            }
+            
+            else {
+                stack.push(c);
+            }
+        }
+
+        return  stack.isEmpty() ? true : false;
+    }
+}
