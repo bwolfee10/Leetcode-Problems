17bb98cf69fef36cf1d00f27dccc4843a1992d4b
Breanna Thompson <35754765+bwolfee10@users.noreply.github.com>
2022-06-01 08:22:07 -0500

    Time: 0 ms (100.00%), Space: 54.4 MB (43.15%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,11 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
         
+        int i = 0;
+        int j = nums.length-1;
         
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
-            }
+        while(i<=j){
+            int mid = (i+j)/2;
+            
+            if(nums[mid] == target) return mid;
+            else if(nums[mid]<target) i = mid+1;
+            else j = mid-1;
         }
         return -1;
     }
